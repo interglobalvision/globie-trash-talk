2bcb442115bd4a271a0bc9ec423882b19cdb75e0
Patrick Best <patrickbest@patrickbest.com>
2015-03-26 19:02:49 -0600

    init commit
    need to debug posting to tumblr & set the randomness as env variable

---
+++ README.md
@@ -0,0 +1,9 @@
+# CUT THE LOUD MESS OUT
+
+### and send it over to a tumblr feed. Rember companies make APIs so more content can be created cus data = engagement [or maybe engagement = data]. Anyhow data===$$$.
+
+#Run this shit like:
+
+- Install forever globially via node
+- Get ur variables in a twist
+- `SLACK_API=* CONSUMER_KEY=* CONSUMER_SECRET=* TOKEN=* TOKEN_SECRET=* BLOG=* forever start main.js`
---
+++ main.js
@@ -0,0 +1,88 @@
+// NPM
+
+var slack = require('slack-client'),
+  tumblr = require('tumblr.js'),
+  randomNumber = require('random-number');
+
+// SET UP SOME VARIABLES FROM THE ENV
+
+var slackApi = process.env.SLACK_API;
+
+var oauth = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.TOKEN,
+  token_secret: process.env.TOKEN_SECRET
+};
+
+var blog = process.env.BLOG;
+
+var randomNumberOptions = {
+  min:  1,
+  max:  2,
+  integer: true
+}
+
+// SETUP NUMBER GENERATOR
+
+var getRandom = randomNumber.generator(randomNumberOptions)
+
+// CONNECT TO TUMBLR
+
+var blog = tumblr.createClient(oauth);
+
+blog.userInfo(function(error, data) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+  console.log(data.blogs);
+});
+
+// SETUP SLACK
+
+globieBot = new slack(slackApi, true, false);
+
+// LISTEN FOR SIGNIN
+
+globieBot.on('error', function(hello) {
+
+  console.log(hello);
+
+});
+
+globieBot.on('open', function() {
+
+  console.log('Connected to Slack');
+
+});
+
+// LISTEN FOR MESSAGES
+
+globieBot.on('message', function(message) {
+  channel = globieBot.getChannelGroupOrDMByID(message.channel)
+  if (channel.name === 'b') {
+    var random = getRandom();
+
+    console.log(random);
+
+    if (random === 1) {
+
+      blog.text(blog, {
+        body: message.text
+      }, function(error, result) {
+          if (error) {
+            console.log(error);
+          }
+          console.log(result);
+      });
+
+    }
+//     console.log(message.text);
+  }
+
+});
+
+// CONNECT TO SLACK
+
+globieBot.login();
